4076be39bbc21b409971ccbceee3757ef8c48700
Christoph Berg <christoph.berg@credativ.de>
2022-01-20 14:47:33 +0100

    Convert README.table_log to markdown

--- table_log.h
+++ table_log.h
@@ -2,7 +2,7 @@
  * table_log () -- log changes to another table
  *
  *
- * see README.table_log for details
+ * see README.md for details
  *
  *
  * written by Andreas ' ads' Scherbaum (ads@pgug.de)
